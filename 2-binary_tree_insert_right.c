c848c4ceea022a996e855c14f3ac73d421d499d8
nycolemedina-lab <nycole.medina@gmain.com>
2026-09-05 15:41:07 -0400

    add task 2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -20,7 +20,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	if (parent->right != NULL)
 	{
 		new_node->right = parent->right;
-		new_node-right->parent = new_node;
+		new_node->right->parent = new_node;
 	}
 	parent->right = new_node;
 
