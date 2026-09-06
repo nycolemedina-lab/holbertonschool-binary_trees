f1b9fe24e34adadac433dd1d10bc4eef64d6c467
nycolemedina-lab <nycole.medina@gmain.com>
2026-09-06 00:11:45 -0400

    add task 17

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+/**
+* binary_tree_sibling - finds the sibling of node
+* @node: pointer to the node to find the sibling of
+*
+* Return: pointer to the sibling node, or null if node or parent null
+*	or if node has no sibling
+*/
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node == NULL)
+		return (NULL);
+
+	if (ndoe->parent == NULL)
+		return (NULL);
+
+	if (node->parent->lerf == node)
+		return (node->parent->right);
+
+	return (node->parent->left);
+}
