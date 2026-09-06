fca2862a2099b4009acc37d1bb736c431fa13bdf
nycolemedina-lab <nycole.medina@gmain.com>
2026-09-05 20:31:15 -0400

    add task 14

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+/**
+* binary_tree_balance - measure the balance factor of a binary tree
+* @tree: pointer to the root node of the tree to measure the balace
+*
+* Return: balance factor, or 0 if tree null
+*/
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int left_h = 0;
+	int right_h = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	left_h = (int)tree_height(tree->left);
+	right_h = (int)tree_height(tree->right);
+
+	return (left_h - right_h);
+}
