a1d977abeb7c5707dcfee021e50a3173cfb1ec0a
nycolemedina-lab <nycole.medina@gmain.com>
2026-09-05 21:11:52 -0400

    add task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,12 +1,12 @@
 #include "binary_trees.h"
 
 /**
-* binary_tree_height - measures the height of a binary tree
+* tree_height - measures the height of a binary tree
 * @tree: pointer to the root node of the tree to measure the height
 *
-* Return: height of the tree if tree is null
+* Return: height of the tree, 0 if tree is null
 */
-size_t binary_tree_height(const binary_tree_t *tree)
+size_t tree_height(const binary_tree_t *tree)
 {
 	size_t left_h = 0;
 	size_t right_h = 0;
